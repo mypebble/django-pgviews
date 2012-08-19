799c06ae3b4d7c36b8a775d4aadc25a01d19605d
Zachary Voase <z@zacharyvoase.com>
2012-08-19 07:17:57 +0000

    Added setuptools boilerplate.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-postgres',
+    version='0.0.1',
+    description="First-class Postgres feature support for the Django ORM.",
+    author='Zachary Voase',
+    author_email='z@zacharyvoase.com',
+    license='Public Domain',
+    packages=find_packages(),
+)
