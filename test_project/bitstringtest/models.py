d992090c3a5ee77eb4f7be5a0f5e2018aa2d6b9a
Zachary Voase <z@zacharyvoase.com>
2012-08-25 22:10:09 +0000

    Added tests for varying and null bitmaps.

--- test_project/bitstringtest/models.py
+++ test_project/bitstringtest/models.py
@@ -5,3 +5,13 @@ import django_postgres
 class BloomFilter(models.Model):
     name = models.CharField(max_length=100)
     bitmap = django_postgres.BitStringField(max_length=8)
+
+
+class VarBitmap(models.Model):
+    name = models.CharField(max_length=100)
+    bitmap = django_postgres.BitStringField(max_length=8, varying=True)
+
+
+class NullBitmap(models.Model):
+    name = models.CharField(max_length=100)
+    bitmap = django_postgres.BitStringField(max_length=8, null=True)
--- test_project/bitstringtest/tests.py
+++ test_project/bitstringtest/tests.py
@@ -11,6 +11,10 @@ class SimpleTest(TestCase):
         # Default bit string is all zeros.
         assert bloom.bitmap.bin == ('0' * 8)
 
+    def test_null_bitmap_defaults_to_None(self):
+        bloom = models.NullBitmap.objects.create(name='foo')
+        assert bloom.bitmap is None
+
     def test_can_change_bitstrings(self):
         bloom = models.BloomFilter.objects.create(name='foo')
         bloom.bitmap = Bits(bin='01011010')
@@ -19,12 +23,38 @@ class SimpleTest(TestCase):
         refetch = models.BloomFilter.objects.get(id=bloom.id)
         assert refetch.bitmap.bin == '01011010'
 
+    def test_can_set_null_bitmap_to_None(self):
+        bloom = models.NullBitmap.objects.create(name='foo',
+                                                 bitmap=Bits(bin='01010101'))
+        assert bloom.bitmap is not None
+        bloom.bitmap = None
+        bloom.save()
+        bloom = models.NullBitmap.objects.get(id=bloom.id)
+        assert bloom.bitmap is None
+
     def test_can_search_for_equal_bitstrings(self):
         models.BloomFilter.objects.create(name='foo', bitmap='01011010')
 
         results = models.BloomFilter.objects.filter(bitmap='01011010')
         assert results.count() == 1
         assert results[0].name == 'foo'
+
+
+class VarBitTest(TestCase):
+
+    def test_can_create_varying_length_bitstrings(self):
+        bloom = models.VarBitmap.objects.create(name='foo')
+        # Default varbit string is one zero.
+        assert bloom.bitmap.bin == '0'
+
+    def test_can_change_varbit_length(self):
+        bloom = models.VarBitmap.objects.create(name='foo',
+                                                bitmap=Bits(bin='01010'))
+        assert len(bloom.bitmap.bin) == 5
+        bloom.bitmap = Bits(bin='0101010')
+        bloom.save()
+        bloom = models.VarBitmap.objects.get(id=bloom.id)
+        assert len(bloom.bitmap.bin) == 7
 
 
 class BitStringExpressionUpdateTest(TestCase):
