ef97a0381da4cededbf6ed4759ab25cc2978ab08
Scott Walton <scw@talktopebble.co.uk>
2012-12-14 14:42:28 +0000

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-postgres',
-    version='0.0.1',
+    version='0.0.2',
     description="First-class Postgres feature support for the Django ORM.",
     author='Zachary Voase',
     author_email='z@zacharyvoase.com',
