e07210a9f896f838fb9760fa7aeef5a0e0f63d6f
Zachary Voase <z@zacharyvoase.com>
2013-01-02 04:15:37 +0000

    Cleaned up the view syncing code, adding more edge-case handling.

--- django_postgres/management/commands/sync_pgviews.py
+++ django_postgres/management/commands/sync_pgviews.py
@@ -1,21 +1,53 @@
-"""Synchronise SQL Views.
-"""
+from optparse import make_option
+import logging
+
 from django.core.management.base import NoArgsCommand
 from django.db import models
 
 from django_postgres.view import create_views
 
 
-class Command(NoArgsCommand):
-    help = 'Creates and Updates all SQL Views'
+log = logging.getLogger('django_postgres.sync_pgviews')
 
-    def handle_noargs(self, **options):
-        all_modules = models.get_apps()
-        modules = '\n '.join((m.__name__ for m in all_modules))
-        self.stdout.write(
-            'Creating Views for all modules:\n {modules}\n'.format(
-                modules=modules
-            )
-        )
-        for module in all_modules:
-            create_views(module)
+
+class Command(NoArgsCommand):
+    help = """Create/update Postgres views for all installed apps."""
+    option_list = NoArgsCommand.option_list + (
+        make_option('--no-update',
+                    action='store_false',
+                    dest='update',
+                    default=True,
+                    help="""Don't update existing views, only create new ones."""),
+        make_option('--force',
+                    action='store_true',
+                    dest='force',
+                    default=False,
+                    help="""Force replacement of pre-existing views where
+                    breaking changes have been made to the schema."""),
+    )
+
+    def handle_noargs(self, force, update, **options):
+        for module in models.get_apps():
+            log.info("Creating views for %s", module.__name__)
+            try:
+                for status, view_cls, python_name in create_views(module, update=update, force=force):
+                    if status == 'CREATED':
+                        msg = "created"
+                    elif status == 'UPDATED':
+                        msg = "updated"
+                    elif status == 'EXISTS':
+                        msg = "already exists, skipping"
+                    elif status == 'FORCED':
+                        msg = "forced overwrite of existing schema"
+                    elif status == 'FORCE_REQUIRED':
+                        msg = "exists with incompatible schema, --force required to update"
+                    log.info("%(python_name)s (%(view_name)s): %(msg)s" % {
+                        'python_name': python_name,
+                        'view_name': view_cls._meta.db_table,
+                        'msg': msg})
+            except Exception, exc:
+                if not hasattr(exc, 'view_cls'):
+                    raise
+                log.exception("Error creating view %s (%r)",
+                              exc.python_name,
+                              exc.view_cls._meta.db_table)
--- django_postgres/view.py
+++ django_postgres/view.py
@@ -2,16 +2,20 @@
 
 import collections
 import copy
+import logging
 import re
 
 from django.db import connection, transaction
 from django.db import models
+import psycopg2
 
 
 FIELD_SPEC_REGEX = (r'^([A-Za-z_][A-Za-z0-9_]*)\.'
                     r'([A-Za-z_][A-Za-z0-9_]*)\.'
                     r'(\*|(?:[A-Za-z_][A-Za-z0-9_]*))$')
 FIELD_SPEC_RE = re.compile(FIELD_SPEC_REGEX)
+
+log = logging.getLogger('django_postgres.view')
 
 
 def hasfield(model_cls, field_name):
@@ -51,26 +55,74 @@ def realize_deferred_projections(sender, *args, **kwargs):
 models.signals.class_prepared.connect(realize_deferred_projections)
 
 
-def create_views(models_module, *args, **kwargs):
-    """Create the database views after syncdb."""
+def create_views(models_module, update=True, force=False):
+    """Create the database views for a given models module."""
     for name, view_cls in vars(models_module).iteritems():
         if not (isinstance(view_cls, type) and
                 issubclass(view_cls, View) and
                 hasattr(view_cls, 'sql')):
             continue
 
-        query = 'CREATE OR REPLACE VIEW {table} AS {select};'
-
-        query = query.format(
-            table=view_cls._meta.db_table,
-            select=view_cls.sql)
-
-        cursor = connection.cursor()
         try:
-            cursor.execute(query)
-            transaction.commit_unless_managed()
-        finally:
-            cursor.close()
+            created = create_view(connection, view_cls._meta.db_table,
+                                  view_cls.sql, update=update, force=force)
+        except Exception, exc:
+            exc.view_cls = view_cls
+            exc.python_name = models_module.__name__ + '.' + name
+            raise
+        else:
+            yield created, view_cls, models_module.__name__ + '.' + name
+
+
+def create_view(connection, view_name, view_query, update=True, force=False):
+    """
+    Create a named view on a connection.
+
+    Returns True if a new view was created (or an existing one updated), or
+    False if nothing was done.
+
+    If ``update`` is True (default), attempt to update an existing view. If the
+    existing view's schema is incompatible with the new definition, ``force``
+    (default: False) controls whether or not to drop the old view and create
+    the new one.
+    """
+    cursor_wrapper = connection.cursor()
+    cursor = cursor_wrapper.cursor.cursor
+    try:
+        force_required = False
+        # Determine if view already exists.
+        cursor.execute('SELECT COUNT(*) FROM pg_catalog.pg_class WHERE relname = %s;',
+                       [view_name])
+        view_exists = cursor.fetchone()[0] > 0
+        if view_exists and not update:
+            return 'EXISTS'
+        elif view_exists:
+            # Detect schema conflict by copying the original view, attempting to
+            # update this copy, and detecting errors.
+            cursor.execute('CREATE TEMPORARY VIEW check_conflict AS SELECT * FROM {0};'.format(view_name))
+            try:
+                cursor.execute('CREATE OR REPLACE TEMPORARY VIEW check_conflict AS {0};'.format(view_query))
+            except psycopg2.ProgrammingError:
+                force_required = True
+                cursor.connection.rollback()
+            finally:
+                cursor.execute('DROP VIEW IF EXISTS check_conflict;')
+
+        if not force_required:
+            cursor.execute('CREATE OR REPLACE VIEW {0} AS {1};'.format(view_name, view_query))
+            ret = view_exists and 'UPDATED' or 'CREATED'
+        elif force:
+            cursor.execute('DROP VIEW {0};'.format(view_name))
+            cursor.execute('CREATE VIEW {0} AS {1};'.format(view_name, view_query))
+            ret = 'FORCED'
+        else:
+            ret = 'FORCE_REQUIRED'
+
+        transaction.commit_unless_managed()
+        return ret
+    finally:
+        cursor_wrapper.close()
+
 
 
 def get_fields_by_name(model_cls, *field_names):
