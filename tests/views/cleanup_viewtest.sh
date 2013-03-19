d31b5094af4e4ac8815dd7b8253efc2a6c1f01a4
Zachary Voase <z@zacharyvoase.com>
2013-03-19 13:08:23 -0700

    Fixed cleanup_viewtest.sh

--- tests/views/cleanup_viewtest.sh
+++ tests/views/cleanup_viewtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATABASE_URL="postgresql://localhost/django_postgres_viewtest"
-rm -Rf view_project_test/
+rm -Rf test__views/
 DROP_CMD=$(psql -P format=unaligned -P tuples_only -d "$DATABASE_URL" -c "select 'DROP TABLE \"' || array_to_string(array_agg(tablename), '\", \"') || '\" CASCADE;' from pg_tables where schemaname = 'public';")
 echo "$DROP_CMD"
 psql -d "$DATABASE_URL" -c "$DROP_CMD"
