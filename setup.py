46020e11ecee9132809ffefd9a47b6750a744a91
Scott Walton <scott.walton@mypebble.co.uk>
2014-07-04 11:42:07 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-pgviews',
-    version='0.0.1',
+    version='0.0.2',
     description="Create and manage Postgres SQL Views in Django",
     author='Scott Walton',
     author_email='scott.walton@mypebble.co.uk',
