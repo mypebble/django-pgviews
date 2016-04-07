e131c4c0fd9634f6dfc6260358df279a451761ea
Scott Walton <scott.walton@mypebble.co.uk>
2016-04-07 10:49:02 +0100

    Bump version for new PyPI readme

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup, find_packages
 
 LONG_DESCRIPTION = ''
 try:
-    LONG_DESCRIPTION = open('README.md').read()
+    LONG_DESCRIPTION = open('README.txt').read()
 except:
     pass
 
 setup(
     name='django-pgviews',
-    version='0.0.7',
+    version='0.0.8',
     description="Create and manage Postgres SQL Views in Django",
     long_description=LONG_DESCRIPTION,
     author='Scott Walton',
     author_email='scott.walton@mypebble.co.uk',
     license='Public Domain',
     packages=find_packages(),
+    url='https://github.com/mypebble/django-pgviews'
 )
