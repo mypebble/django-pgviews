0dd26cdf702d78d0dda520eaf0488fe88f39cd87
Scott Walton <scott.walton@mypebble.co.uk>
2017-01-10 10:57:14 +0000

    Update version

--- setup.py
+++ setup.py
@@ -8,12 +8,21 @@ except:
 
 setup(
     name='django-pgviews',
-    version='0.2.0',
+    version='0.3.0',
     description="Create and manage Postgres SQL Views in Django",
     long_description=LONG_DESCRIPTION,
     author='Scott Walton',
     author_email='scott.walton@mypebble.co.uk',
     license='Public Domain',
     packages=find_packages(),
-    url='https://github.com/mypebble/django-pgviews'
+    url='https://github.com/mypebble/django-pgviews',
+    classifiers=[
+        'Development Status :: 5 - Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+    ]
 )
