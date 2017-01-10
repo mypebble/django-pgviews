a8e597f4191c48f3d1efa3c28bb54ada3ce8b912
Scott Walton <scott.walton@mypebble.co.uk>
2017-01-10 11:00:04 +0000

    Fix classifier

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(),
     url='https://github.com/mypebble/django-pgviews',
     classifiers=[
-        'Development Status :: 5 - Stable',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
