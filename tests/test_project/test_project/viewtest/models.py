9cdd35642401367b76203f1574849b02b4d17d79
Scott Walton <scott.walton@mypebble.co.uk>
2017-10-19 08:42:26 +0100

    Slightly nicer imports for testing

--- tests/test_project/test_project/viewtest/models.py
+++ tests/test_project/test_project/viewtest/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django_pgviews import view as django_pgviews
+from django_pgviews import view
 
 
 class TestModel(models.Model):
@@ -9,12 +9,12 @@ class TestModel(models.Model):
     name = models.CharField(max_length=100)
 
 
-class Superusers(django_pgviews.View):
+class Superusers(view.View):
     projection = ['auth.User.*']
     sql = """SELECT * FROM auth_user WHERE is_superuser = TRUE;"""
 
 
-class SimpleUser(django_pgviews.View):
+class SimpleUser(view.View):
     projection = ['auth.User.username', 'auth.User.password']
     # The row_number() window function is needed so that Django sees some kind
     # of 'id' field. We could also grab the one from `auth.User`, but this
@@ -27,31 +27,33 @@ class SimpleUser(django_pgviews.View):
     FROM auth_user;"""
 
 
-class RelatedView(django_pgviews.ReadOnlyView):
+class RelatedView(view.ReadOnlyView):
+    sql = """SELECT id AS model_id, id FROM viewtest_testmodel"""
+    model = models.ForeignKey(TestModel)
+
+
+class MaterializedRelatedView(view.ReadOnlyMaterializedView):
     sql = """SELECT id AS model_id, id FROM viewtest_testmodel"""
     model = models.ForeignKey(TestModel, on_delete=models.DO_NOTHING)
 
 
-class MaterializedRelatedView(django_pgviews.ReadOnlyMaterializedView):
-    sql = """SELECT id AS model_id, id FROM viewtest_testmodel"""
-    model = models.ForeignKey(TestModel, on_delete=models.DO_NOTHING)
-
-
-class DependantView(django_pgviews.ReadOnlyView):
+class DependantView(view.ReadOnlyView):
     dependencies = ('viewtest.RelatedView',)
     sql = """SELECT model_id from viewtest_relatedview;"""
 
 
-class DependantMaterializedView(django_pgviews.ReadOnlyMaterializedView):
+class DependantMaterializedView(view.ReadOnlyMaterializedView):
     dependencies = ('viewtest.MaterializedRelatedView',)
     sql = """SELECT model_id from viewtest_materializedrelatedview;"""
 
-class MaterializedRelatedViewWithIndex(django_pgviews.ReadOnlyMaterializedView):
+
+class MaterializedRelatedViewWithIndex(view.ReadOnlyMaterializedView):
     concurrent_index = 'id'
     sql = """SELECT id AS model_id, id FROM viewtest_testmodel"""
     model = models.ForeignKey(TestModel, on_delete=models.DO_NOTHING)
 
-class CustomSchemaView(django_pgviews.ReadOnlyView):
+
+class CustomSchemaView(view.ReadOnlyView):
     sql = """SELECT id AS model_id, id FROM viewtest_testmodel"""
     model = models.ForeignKey(TestModel, on_delete=models.DO_NOTHING)
 
